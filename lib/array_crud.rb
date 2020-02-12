a7164c0953fc2afc022368de43d75e7719bc6cff
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-12 20:50:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  [cats,dogs,fish,turtles]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << [chickens]
 end
 
 def add_element_to_start_of_array(array, element)
